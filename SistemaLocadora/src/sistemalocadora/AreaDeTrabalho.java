36fa8b94d8a2d93b3631b8b5ae6a64e474bf50b2
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2019-08-14 20:55:30 -0300

    Sistema Locadora

---
+++ SistemaLocadora/src/sistemalocadora/AreaDeTrabalho.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemalocadora;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class AreaDeTrabalho extends JDesktopPane {
+    private JInternalFrame janelaCliente;
+    
+    
+    public void abrirJanelaCliente() {
+        if (janelaCliente == null) {
+            janelaCliente = new JanelaCliente();
+            janelaCliente.setVisible(true);
+            this.add(janelaCliente);
+        }
+    }
+    
+    public void fecharJanelaCliente() {
+        janelaCliente = null;
+    }
+}
